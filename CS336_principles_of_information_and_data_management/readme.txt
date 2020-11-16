2d0e99b847866241369a2ffc6d25daa5dc076de0
Austin <austinbennett1398@gmail.com>
2020-11-16 13:24:35 -0500

    asd

--- CS336_principles_of_information_and_data_management/readme.txt
+++ CS336_principles_of_information_and_data_management/readme.txt
@@ -1,1 +1,3 @@
-Unfortunately Amazon has started to charge me for continuing to host the website my group created for this project so there is no visual available but I have provided all of the code for the webpages.
+version https://git-lfs.github.com/spec/v1
+oid sha256:d426e0223e7ce7090fc2c34fddd6adb77252f9bdda0b6fd79a7476b6dbb03c58
+size 200
--- CS440_artificial_intelligence/final_project/code_etc/plot.py
+++ CS440_artificial_intelligence/final_project/code_etc/plot.py
@@ -1,36 +1,3 @@
-import matplotlib.pyplot as plt
-
-def plot_digits(y, descriptor = 'accuracy'):
-    digits = [500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000]
-    plt.plot(digits, y)
-    plt.xlabel('# of data points used')
-    if(descriptor == 'accuracy'):
-        plt.ylabel('Test Accuracy %')
-        plt.title('K-Nearest Neighbor test accuracy per sample size')
-    else:
-        plt.ylabel('Time in seconds')
-        plt.title('K-Nearest Neighbor time per sample size')
-    plt.show()
-
-def plot_faces(y, descriptor = 'accuracy'):
-    faces = [45, 90, 135, 180, 225, 270, 315, 360, 405, 451]
-    plt.plot(faces, y)
-    plt.xlabel('# of data points used')
-    if(descriptor == 'accuracy'):
-        plt.ylabel('Test Accuracy %')
-        plt.title('Naive Bayes test accuracy per sample size')
-    else:
-        plt.ylabel('Time in seconds')
-        plt.title('Naive Bayes time per sample size')
-    plt.show()
-
-
-
-if __name__ == '__main__':
-    time = [447,	890	,1327,	1798,	2327,	2755,	3417,	3853	,4065,	4510]
-    accuracy = [61.3,	66.6,	70	,70.8	,73.3,	73.5,	75	,76,	77.5,	78.2]
-    plot_digits(time, descriptor = 'time')
-    plot_digits(accuracy, descriptor = 'accuracy')
-
-    # plot_faces(time, descriptor = 'time')
-    # plot_faces(accuracy, descriptor = 'accuracy')
+version https://git-lfs.github.com/spec/v1
+oid sha256:c422357808075f067179bba8677148f030195894aff9fbfe9ee51e77cf73a243
+size 1297
